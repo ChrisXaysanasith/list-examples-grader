dec75ddd0e4440a03cd1ad1542a17b0edd3544bd
Chris <cxaysanasith@ucsd.edu>
2023-11-06 18:08:40 -0800

    changed

--- grade.sh
+++ grade.sh
@@ -1,4 +1,4 @@
-CPATH='.:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar'
+CPATH=".;../lib/hamcrest-core-1.3.jar;../lib/junit-4.13.2.jar"
 
 rm -rf student-submission
 rm -rf grading-area
@@ -8,6 +8,17 @@ mkdir grading-area
 git clone $1 student-submission
 echo 'Finished cloning'
 
+if [[ ! -f student-submission/ListExamples.java ]] 
+then echo "Wrong File"
+    exit 1
+fi 
+
+cp student-submission/ListExamples.java grading-area/
+cp TestListExamples.java grading-area/
+cd grading-area
+
+javac -cp $CPATH ListExamples.java TestListExamples.java
+java -cp $CPATH org.junit.runner.JUnitCore TestListExamples
 
 # Draw a picture/take notes on the directory structure that's set up after
 # getting to this point
